bd5fb3887e8e84325a6c96f6df1838ffc2778b9f
ASUS <khattar.harshita242@gmail.com>
2024-10-09 17:43:08 +0530

    project started

---
+++ src/UI/Components/LandingPage/landingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./landingPage.css";
+
+export const LandingPage = () =>{
+    return (
+        <div id="mainContainer">
+            hello
+        </div>
+    );
+}
+
+export default LandingPage;
